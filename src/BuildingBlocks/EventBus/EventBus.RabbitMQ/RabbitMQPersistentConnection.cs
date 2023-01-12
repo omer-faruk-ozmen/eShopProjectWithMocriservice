04253125026ca044b7411b0cddbe669691b9aa38
Ömer Faruk Özmen <omerfarukozmen0@gmail.com>
2023-01-12 12:28:35 +0300

    trivial

--- src/BuildingBlocks/EventBus/EventBus.RabbitMQ/RabbitMQPersistentConnection.cs
+++ src/BuildingBlocks/EventBus/EventBus.RabbitMQ/RabbitMQPersistentConnection.cs
@@ -1,89 +1,83 @@
 ﻿using Polly;
 using RabbitMQ.Client;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 using RabbitMQ.Client.Exceptions;
 
-namespace EventBus.RabbitMQ
+namespace EventBus.RabbitMQ;
+
+public class RabbitMqPersistentConnection : IDisposable
 {
-    public class RabbitMqPersistentConnection : IDisposable
+    private IConnection _connection;
+    private readonly IConnectionFactory _connectionFactory;
+    private object lock_object = new object();
+    private int retryCount;
+    private bool _disposed;
+
+    public RabbitMqPersistentConnection(IConnectionFactory connectionFactory, int retryCount = 5)
     {
-        private IConnection _connection;
-        private readonly IConnectionFactory _connectionFactory;
-        private object lock_object = new object();
-        private int retryCount;
-        private bool _disposed;
+        _connectionFactory = connectionFactory;
+        this.retryCount = retryCount;
+    }
 
-        public RabbitMqPersistentConnection(IConnectionFactory connectionFactory, int retryCount = 5)
-        {
-            _connectionFactory = connectionFactory;
-            this.retryCount = retryCount;
-        }
+    public bool IsConnection => _connection != null && _connection.IsOpen;
 
-        public bool IsConnection => _connection != null && _connection.IsOpen;
+    public IModel CreateModel()
+    {
+        return _connection.CreateModel();
+    }
+    public void Dispose()
+    {
+        _disposed = true;
+        _connection.Dispose();
+    }
 
-        public IModel CreateModel()
+    public bool TryConnect()
+    {
+        lock (lock_object)
         {
-            return _connection.CreateModel();
-        }
-        public void Dispose()
-        {
-            _disposed = true;
-            _connection.Dispose();
-        }
+            var policy = Policy.Handle<SocketException>()
+                .Or<BrokerUnreachableException>()
+                .WaitAndRetry(retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    (ex, time) =>
+                    {
 
-        public bool TryConnect()
-        {
-            lock (lock_object)
+                    });
+
+            policy.Execute(() =>
             {
-                var policy = Policy.Handle<SocketException>()
-                    .Or<BrokerUnreachableException>()
-                    .WaitAndRetry(retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
-                        (ex, time) =>
-                        {
+                _connection = _connectionFactory.CreateConnection();
+            });
 
-                        });
-
-                policy.Execute(() =>
-                {
-                    _connection = _connectionFactory.CreateConnection();
-                });
-
-                if (IsConnection)
-                {
-                    _connection.ConnectionShutdown += Connection_ConnectionShutdown;
-                    _connection.CallbackException += Connection_CallbackException;
-                    _connection.ConnectionBlocked += Connection_ConnectionBlocked;
-                    //log
-                    return true;
-                }
-
-                return false;
+            if (IsConnection)
+            {
+                _connection.ConnectionShutdown += Connection_ConnectionShutdown;
+                _connection.CallbackException += Connection_CallbackException;
+                _connection.ConnectionBlocked += Connection_ConnectionBlocked;
+                //log
+                return true;
             }
 
+            return false;
         }
 
-        private void Connection_ConnectionBlocked(object? sender, global::RabbitMQ.Client.Events.ConnectionBlockedEventArgs e)
-        {
-            if (_disposed) return;
-            TryConnect();
-        }
+    }
 
-        private void Connection_CallbackException(object? sender, global::RabbitMQ.Client.Events.CallbackExceptionEventArgs e)
-        {
-            if (_disposed) return;
-            TryConnect();
-        }
+    private void Connection_ConnectionBlocked(object? sender, global::RabbitMQ.Client.Events.ConnectionBlockedEventArgs e)
+    {
+        if (_disposed) return;
+        TryConnect();
+    }
 
-        private void Connection_ConnectionShutdown(object? sender, ShutdownEventArgs e)
-        {
-            if (_disposed) return;
-            //log
-            TryConnect();
-        }
+    private void Connection_CallbackException(object? sender, global::RabbitMQ.Client.Events.CallbackExceptionEventArgs e)
+    {
+        if (_disposed) return;
+        TryConnect();
+    }
+
+    private void Connection_ConnectionShutdown(object? sender, ShutdownEventArgs e)
+    {
+        if (_disposed) return;
+        //log
+        TryConnect();
     }
 }
